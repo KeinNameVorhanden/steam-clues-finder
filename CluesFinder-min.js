02719003a68d9fa0cd98419722d54b8210beb006
www <l2u0k0a0s.ld@gmail.com>
2022-06-24 16:07:55 +0200

    Initial commit

---
+++ CluesFinder-min.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Clues Finder
+// @namespace    CluesFinderSteam
+// @version      0.2
+// @description  Will find the clues.
+// @author       KeinNameVorhanden
+// @match        https://store.steampowered.com/category/*/*
+// @match        https://store.steampowered.com/vr/*
+// @match        https://store.steampowered.com/sale/clorthax_quest*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @grant        none
+// ==/UserScript==
+
+let once=!1;function goto_clue(){console.log(window.location.href.includes("sale/clorthax_quest")),window.location.href.includes("sale/clorthax_quest")&&setTimeout(function(){let a=document.getElementsByClassName("LinkButton");"Don't Click Me"===a[1].text||"Nicht hier klicken"===a[1].text?(document.getElementsByClassName("DialogButton _DialogLayout Secondary Focusable")[0].click(),setTimeout(function(){a[1].click()},500)):a[1].click()},500)}function add_onclick(){window.location.href.includes("sale/clorthax_quest")||setTimeout(function(){document.getElementsByClassName("SaleTab Selected")[0].addEventListener("click",find_game)},500)}function find_game(){if(!window.location.href.includes("sale/clorthax_quest")){let a=document.getElementsByClassName("sale_page_background")[0].scrollHeight;window.scrollTo({top:a,left:0,behavior:"smooth"}),setTimeout(function(){let b=document.getElementsByClassName("ImpressionTrackedElement");for(var a=0;a<b.length;a++)if(b[a].textContent.includes("CL.0RTH.4X")){!1==once&&(once=!0),b[a].scrollIntoView(),window.scrollBy(0,-300);break}console.log("Script just ran")},1200)}}(void(add_onclick(),find_game(),goto_clue()))(function(a){var b=a.pushState;a.pushState=function(c){return"function"==typeof a.onpushstate&&a.onpushstate({state:c}),b.apply(a,arguments)}})(window.history),window.onpopstate=history.onpushstate=function(a){once=!1,add_onclick(),find_game(),goto_clue()}
---
+++ CluesFinder.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name         Clues Finder
+// @namespace    CluesFinderSteam
+// @version      0.2
+// @description  Will find the clues.
+// @author       KeinNameVorhanden
+// @match        https://store.steampowered.com/category/*/*
+// @match        https://store.steampowered.com/vr/*
+// @match        https://store.steampowered.com/sale/clorthax_quest*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @grant        none
+// ==/UserScript==
+
+let once = false
+
+function goto_clue() {
+    console.log((window.location.href).includes("sale/clorthax_quest"))
+    if((window.location.href).includes("sale/clorthax_quest")) {
+        setTimeout(function() {
+            let buttons = document.getElementsByClassName("LinkButton")
+            if (buttons[1].text === "Don't Click Me" || buttons[1].text === "Nicht hier klicken") {
+                let button = document.getElementsByClassName("DialogButton _DialogLayout Secondary Focusable")[0].click()
+                setTimeout(function() {
+                    buttons[1].click()
+                }, 500)
+            } else {
+                buttons[1].click()
+            }
+        }, 500)
+    }
+}
+
+function add_onclick() {
+    if(!((window.location.href).includes("sale/clorthax_quest"))) {
+        setTimeout(function() {
+            let tab = document.getElementsByClassName("SaleTab Selected")[0].addEventListener("click", find_game)
+            }, 500)
+    }
+}
+
+function find_game() {
+    if(!((window.location.href).includes("sale/clorthax_quest"))) {
+        let page = document.getElementsByClassName("sale_page_background")[0].scrollHeight
+        window.scrollTo({
+            top: page,
+            left: 0,
+            behavior: 'smooth'
+        })
+
+        setTimeout(function() {
+            let e = document.getElementsByClassName("ImpressionTrackedElement") //CL.0RTH.4X
+            for(var j = 0; j < e.length; j++) {
+                if (e[j].textContent.includes("CL.0RTH.4X")) {
+                    if(once == false) {
+                        //alert("Found the game")
+                        once = true
+                    }
+                    e[j].scrollIntoView()
+                    window.scrollBy(0, -300)
+                    break
+                }
+            }
+            console.log("Script just ran")
+        }, 1200)
+    }
+}
+
+(function() {
+    add_onclick()
+    find_game()
+    goto_clue()
+})()
+
+(function(history) {
+    //console.log("[Hide Progress] history");
+    var pushState = history.pushState
+    history.pushState = function(state) {
+        if (typeof history.onpushstate == "function") {
+            history.onpushstate({state: state})
+        }
+        return pushState.apply(history, arguments)
+    }
+})(window.history)
+
+window.onpopstate = history.onpushstate = function(e) {
+    once = false
+    add_onclick()
+    find_game()
+    goto_clue()
+}
