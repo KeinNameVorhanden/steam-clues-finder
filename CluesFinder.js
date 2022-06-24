4a684cd296f4b165f561251f28a0f5c9a271e6c1
www <l2u0k0a0s.ld@gmail.com>
2022-06-24 17:43:05 +0200

    0.3

--- CluesFinder.js
+++ CluesFinder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clues Finder
 // @namespace    CluesFinderSteam
-// @version      0.2
+// @version      0.3
 // @description  Will find the clues.
 // @author       KeinNameVorhanden
 // @match        https://store.steampowered.com/category/*/*
@@ -11,7 +11,7 @@
 // @grant        none
 // ==/UserScript==
 
-let once = false
+let once = false;
 
 function goto_clue() {
     if((window.location.href).includes("sale/clorthax_quest")) {
@@ -21,21 +21,21 @@ function goto_clue() {
                 let button = document.getElementsByClassName("DialogButton _DialogLayout Secondary Focusable")[0].click()
                 setTimeout(function() {
                     buttons[1].click()
-                }, 500)
+                }, 500);
             } else {
                 buttons[1].click()
             }
-        }, 500)
+        }, 500);
     }
-}
+};
 
 function add_onclick() {
     if(!((window.location.href).includes("sale/clorthax_quest"))) {
         setTimeout(function() {
             let tab = document.getElementsByClassName("SaleTab Selected")[0].addEventListener("click", find_game)
-            }, 500)
+        }, 500);
     }
-}
+};
 
 function find_game() {
     if(!((window.location.href).includes("sale/clorthax_quest"))) {
@@ -44,14 +44,14 @@ function find_game() {
             top: page,
             left: 0,
             behavior: 'smooth'
-        })
+        });
 
         setTimeout(function() {
             let e = document.getElementsByClassName("ImpressionTrackedElement") //CL.0RTH.4X
             for(var j = 0; j < e.length; j++) {
                 if (e[j].textContent.includes("CL.0RTH.4X")) {
                     if(once == false) {
-                        alert("Found the game")
+                        //alert("Found the game")
                         once = true
                     }
                     e[j].scrollIntoView()
@@ -60,15 +60,15 @@ function find_game() {
                     break
                 }
             }
-        }, 1200)
+        }, 1200);
     }
-}
+};
 
 (function() {
     add_onclick()
     find_game()
     goto_clue()
-})()
+})();
 
 (function(history) {
     var pushState = history.pushState
@@ -78,11 +78,11 @@ function find_game() {
         }
         return pushState.apply(history, arguments)
     }
-})(window.history)
+})(window.history);
 
 window.onpopstate = history.onpushstate = function(e) {
     once = false
     add_onclick()
     find_game()
     goto_clue()
-}
+};
